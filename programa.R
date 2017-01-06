498e8e6c0de6031030d7d3c2b1eed178753b4945
Aldaamaa <mariaaldama@opendeusto.es>
2017-01-06 22:56:57 +0100

    Code

---
+++ programa.R
@@ -0,0 +1,6 @@
+## Acceder al repositorio donde se encuentran los archivos csv
+setwd("C:/Users/Mara/Desktop/EjerciciosIA/Proyecto/SombreroHP")
+
+## Leer archivos csv
+datos_pre = read.csv("preguntas.csv")
+datos_res = read.csv("respuestas.csv")
