ae2bae1fb1e047e81ed555797e87aa9540e0f740
Aldaamaa <mariaaldama@opendeusto.es>
2017-01-07 00:41:51 +0100

    meh

--- programa.R
+++ programa.R
@@ -8,7 +8,7 @@ datos_res = read.csv("respuestas.csv")
 
 ##Para hacer las preguntas
 p = 10 #preguntas a realizar
-preguntas = sample(1:nrow(datos_pre),10) #Almacenar numeros de preguntas
+preguntas = sort(sample(1:nrow(datos_pre),10)) #Almacenar numeros de preguntas
 respuestas = vector("character",10) #Creamos vector para despues almacenar respuestas
 for(i in 1:p){
   pregunta = levels(drop.levels(datos_pre[preguntas[i],2])) #Accedemos a la descripción de la pregunta
