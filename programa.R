1f8489f54375cfeb4ef070c5593c722d932e8f77
Ekaitz Polledo <ekaitz.polledo@opendeusto.es>
2017-01-10 23:57:55 +0100

    Funciones Bayes

--- programa.R
+++ programa.R
@@ -1,5 +1,6 @@
 ## Acceder al repositorio donde se encuentran los archivos csv
-setwd("C:/Users/Mara/Desktop/EjerciciosIA/Proyecto/SombreroHP/recursos")
+setwd("C:/Users/Ekaitz/Desktop/Dropbox/InteligenciaArtificial/SombreroHP/recursos")
+library(e1071)
 
 ## Leer archivos csv
 datos_pre = read.csv("preguntas.csv")
@@ -11,7 +12,7 @@ p = 10 #preguntas a realizar
 preguntas = sort(sample(1:nrow(datos_pre),10)) #Almacenar numeros de preguntas
 respuestas = vector("character",10) #Creamos vector para despues almacenar respuestas
 for(i in 1:p){
-  pregunta = levels(drop.levels(datos_pre[preguntas[i],2])) #Accedemos a la descripción de la pregunta
+  pregunta = levels(droplevels(datos_pre[preguntas[i],2])) #Accedemos a la descripción de la pregunta
   respuestas[i] = readline(pregunta) #Almacenamos las respuestas del usuario
 }
 
@@ -19,3 +20,5 @@ for(i in 1:p){
 datos_tratar = datos_res[,1:2]
 datos_tratar = cbind(datos_tratar, datos_res[, (preguntas+2)])
 
+#model=naiveBayes(datos_tratar, labels_tra, laplace = 0);
+#out = predict(model, data_tst, type="class")
