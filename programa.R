9efd994b89fba28fc0ba48970f85ec0845412346
Ekaitz Polledo <ekaitz.polledo@opendeusto.es>
2017-01-11 16:50:35 +0100

    aaaaaa

--- programa.R
+++ programa.R
@@ -9,17 +9,26 @@ datos_res = read.csv("respuestas.csv")
 
 ##Para hacer las preguntas
 p = 10 #preguntas a realizar
-preguntas = sort(sample(1:nrow(datos_pre),10)) #Almacenar numeros de preguntas
-respuestas = vector("character",10) #Creamos vector para despues almacenar respuestas
-for(i in 1:p){
+preguntas = sort(sample(1:nrow(datos_pre),p)) #Almacenar numeros de preguntas
+respuestas = vector("character",p) #Creamos vector para despues almacenar respuestas
+i = 1;
+while(i<p){
   pregunta = levels(droplevels(datos_pre[preguntas[i],2])) #Accedemos a la descripción de la pregunta
-  respuestas[i] = readline(pregunta) #Almacenamos las respuestas del usuario
+  respuesta = readline(pregunta); #Almacenamos las respuestas del usuario
+  if(respuesta == "SI" || respuesta == "NO"){
+    respuestas[i] = respuesta;
+    i = i + 1;
+  } else {
+    print("Debes responder SI o NO a la pregunta");
+  }
 }
 
 # Tabla con los datos de los personajes en base a las respuestas del usuario
 datos_tratar = datos_res[,1:2]
 datos_tratar = cbind(datos_tratar, datos_res[, (preguntas+2)])
 
-model=naiveBayes(datos_tratar[,3:10], datos_tratar[,2], laplace = 1);
-out = predict(model, respuestas, type="class");
-tab = table(out, labels_tst)
+model=naiveBayes(datos_tratar[,3:12], datos_tratar[,2], laplace = 1);
+summary(model)
+respuestas =as.data.frame(t(respuestas))
+out = predict(model, respuestas, type="raw");
+out
