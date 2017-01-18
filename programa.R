a425c1a428cc940fb8243c7674fc5c950090fc52
Ekaitz Polledo <ekaitz.polledo@opendeusto.es>
2017-01-18 16:46:03 +0100

    SSS

--- programa.R
+++ programa.R
@@ -48,64 +48,75 @@ for(i in 1:nrow(datos_res)){
 ##Para hacer las preguntas
 p = 10 #preguntas a realizar
 preguntas = sort(sample(1:nrow(datos_pre),p)) #Almacenar numeros de preguntas
-respuestas = vector("character",dim(datos_pre)[1]) #Creamos vector para despues almacenar respuestas
-respuestas[1:dim(datos_pre)[1]]=NA
+#respuestas = vector("character",dim(datos_pre)[1]) #Creamos vector para despues almacenar respuestas (Proceso 1)
+respuestas = vector("character",p)  #(Proceso 2)
+#respuestas[1:dim(datos_pre)[1]]=NA #(Proceso 1)
 i = 1;
 while(i<=p){
   pregunta = levels(droplevels(datos_pre[preguntas[i],2])) #Accedemos a la descripciÃ³n de la pregunta
   respuesta = readline(pregunta); #Almacenamos las respuestas del usuario
   if(respuesta == "SI" || respuesta == "NO"){
-    respuestas[preguntas[i]] = respuesta;
+    #respuestas[preguntas[i]] = respuesta;  #(Proceso 1)
+    respuestas[i] = respuesta;  #(Proceso 2)
     i = i + 1;
   } else {
     print("Debes responder SI o NO a la pregunta");
   }
 }
 
-#respuestas = as.data.frame(t(respuestas));
-#Cuadruplicamos la línea para rellenarlas para cada casa
-#respuestas[2:4,] = respuestas[1,];
+# - Proceso 1 -
 
-respuestas = t(replicate(4, respuestas))
+#respuestas = t(replicate(4, respuestas))
 
 #Rellenamos los huecos vacíos con la ocurrencia mayoritaria en su casa
-for(x in 1:dim(datos_pre)[1]){
-  if(is.na(respuestas[[1,x]])){
-    countCasas = table(datos_res[[x+2]],datos_res$casa);
-    for(y in 1:4){
-      if(countCasas[2*y-1] > countCasas[2*y]){
-        respuestas[y,x] = "NO";
-      } else {
-        respuestas[y,x] = "SI";
-      }
-    }
-    summary(datos_res$casa)
-  }
-}
+#for(x in 1:dim(datos_pre)[1]){
+#  if(is.na(respuestas[[1,x]])){
+#    countCasas = table(datos_res[[x+2]],datos_res$casa);
+#    for(y in 1:4){
+#      if(countCasas[2*y-1] > countCasas[2*y]){
+#        respuestas[y,x] = "NO";
+#      } else {
+#        respuestas[y,x] = "SI";
+#      }
+#    }
+#    summary(datos_res$casa)
+#  }
+#}
 
 #respuestas = as.data.frame((respuestas), stringsAsFactors=TRUE);
 
+
+
+#model=naiveBayes(datos_res[,3:dim(datos_res)[2]], datos_res[,2], laplace = 1);
+#summary(model)
+#respuestas =as.data.frame((respuestas))
+#colnames(respuestas)=colnames(datos_res)[3:dim(datos_res)[2]]
+#out = predict(model, respuestas, type="raw");
+#out
+
+#mediaCasa1 = mean(out[1:4]);
+#mediaCasa2 = mean(out[5:8]);
+#mediaCasa3 = mean(out[9:12]);
+#mediaCasa4 = mean(out[13:16]);
+
+# - Proceso 2 -
+
 # Tabla con los datos de los personajes en base a las respuestas del usuario
-#datos_tratar = datos_res[,1:2]
-#datos_tratar = cbind(datos_tratar, datos_res[, (preguntas+2)])
+datos_tratar = datos_res[,1:2]
+datos_tratar = cbind(datos_tratar, datos_res[, (preguntas+2)])
 
-model=naiveBayes(datos_res[,3:dim(datos_res)[2]], datos_res[,2], laplace = 1);
+model=naiveBayes(datos_tratar[,3:12], datos_tratar[,2], laplace = 1);
 summary(model)
-respuestas =as.data.frame((respuestas))
-colnames(respuestas)=colnames(datos_res)[3:dim(datos_res)[2]]
+respuestas =as.data.frame(t(respuestas));
+colnames(respuestas)=colnames(datos_tratar)[3:12]
 out = predict(model, respuestas, type="raw");
-out
+out 
 
-mediaCasa1 = mean(out[1:4]);
-mediaCasa2 = mean(out[5:8]);
-mediaCasa3 = mean(out[9:12]);
-mediaCasa4 = mean(out[13:16]);
 
 
 #Para el proceso final, revisar la opcion mayoritaria de cada casa en la tabla
 #Si esa es diferente entre las dos opciones, esa será la pregunta a realizar
 
-casa1 = 1;
-casa2 = 3
+
 
 
